4473aeb00a755a259a476baf798cdb0b4f7c66a4
SINJE2 <SINJE2@OOCL.COM>
2025-09-05 11:53:46 +0800

    Test: add test case for countOff when count is 1

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,22 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    public void should_return_normal_number__when_input_normal_number() throws Exception {
+        //Given
+        int input = 1;
+        FizzBuzz fizzbuzz = new FizzBuzz();
+        String expectResult = "1";
+        //When
+        String result = fizzbuzz.countOff(input);
+        //Then
+        assertEquals(expectResult, result);
+    }
+
+
+
 }
